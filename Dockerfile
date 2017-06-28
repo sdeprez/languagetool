8e17784c5e2005e61490f744bd9de62fc473e907
sdeprez <sdeprez@users.noreply.github.com>
2017-06-28 15:21:19 +0200

    Better deploy script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM openjdk:8-jdk
 
-RUN mkdir -p /usr/src/app
-COPY ./languagetool-standalone/target/LanguageTool-3.8-SNAPSHOT/LanguageTool-3.8-SNAPSHOT/ /usr/src/app/
+COPY ./ ./
 
-WORKDIR /usr/src/app
 CMD ["java", "-cp", "languagetool-server.jar", "org.languagetool.server.HTTPServer", "--port", "8081", "--public"]
--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-validate() {
+validate () {
   echo "staging production" | grep -F -q -w "$1";
 }
 
 deploy () {
   IMAGE="madumbo:languagetool-$1"
   echo "Building $IMAGE"
+
+  TARGET_DIR="languagetool-standalone/target/LanguageTool-3.8-SNAPSHOT/LanguageTool-3.8-SNAPSHOT/"
+
+  echo "Removing $TARGET_DIR"
+  rm -rf $TARGET_DIR
+  build
+
+  cp Dockerfile $TARGET_DIR
+  cd $TARGET_DIR
 
   $(aws --region eu-central-1 ecr get-login)
   docker build -t $IMAGE .
@@ -17,6 +26,7 @@ deploy () {
 }
 
 build () {
+  echo "Building target"
   ./build.sh languagetool-standalone package -DskipTests
 }
 
